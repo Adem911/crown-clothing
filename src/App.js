1d70059016671fade299d0ed696023ef6e6d7b49
Adem911 <adempirat99@live.fr>
2020-01-14 15:10:44 +0100

    add withRouter() hoc to our menuItem component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import "./App.css";
+
+const Hats = () => <h1>hats</h1>;
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        {/* Switch render the 1st maching path */}
+        <Route path="/hats" component={Hats} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -43,10 +43,9 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => {
-          return (
-            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
-          );
+        {this.state.sections.map(({ id, ...otherSectionProps }) => {
+          // ...otherSectionProps(...adksjbfbf works aswell ) contains all of state props like: title, size ...
+          return <MenuItem key={id} {...otherSectionProps} />;
         })}
       </div>
     );
