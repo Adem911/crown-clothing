79b58fea5803ef363b2956adb3b04388cc0bbbdd
Adem911 <adempirat99@live.fr>
2020-02-04 11:28:45 +0100

    added shopdata to firebase

--- src/App.js
+++ src/App.js
@@ -9,9 +9,14 @@ import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up
 import CheckoutPage from "./pages/checkout/checkout.component";
 
 import Header from "./components/header/header.component";
-import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import {
+  auth,
+  createUserProfileDocument
+  /*addCollectionAndDocuments*/
+} from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurrentUser } from "./redux/user/user.selectors";
+// import { selectCollectionsForPreview } from "./redux/shop/shop.selectors";
 
 import "./App.css";
 
@@ -19,7 +24,7 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
+    const { setCurrentUser /*collectionsArray*/ } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -34,6 +39,15 @@ class App extends React.Component {
       } else {
         setCurrentUser(userAuth);
       }
+
+      // we did this to add our shopdate to firebase only once
+      //addCollectionAndDocuments(
+      //  "collections",
+      //  collectionsArray.map(({ title, items }) => ({
+      //    title,
+      //    items
+      //  }))
+      //);
     });
   }
 
@@ -43,7 +57,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="app">
         <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
@@ -69,6 +83,7 @@ class App extends React.Component {
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
+  /*collectionsArray: selectCollectionsForPreview*/
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/components/header/header.styles.js
+++ src/components/header/header.styles.js
@@ -10,16 +10,21 @@ import { Link } from "react-router-dom";
 
 export const HeaderContainer = styled.div`
   height: 70px;
-  width: 100%;
   display: flex;
   justify-content: space-between;
-  margin-bottom: 25px;
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  width: 100%;
+  z-index: 999;
+  background-color: #e6e6e6;
 `;
 
 export const LogoContainer = styled(Link)`
   height: 100%;
   width: 70px;
-  padding: 25px;
+  padding: 15px 30px;
 `;
 
 export const OptionsContainer = styled.div`
@@ -28,12 +33,16 @@ export const OptionsContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  margin-right: 10px;
 `;
 
 export const OptionLink = styled(Link)`
-  padding: 10px 15px;
+  padding: 24.5px 15px;
   cursor: pointer;
   margin: 0 5px;
+  &:hover {
+    background-color: white;
+  }
 `;
 // we can render <OptionLink as='div'> and it will give us a div with same style as OtionLink
 // example in header.component
